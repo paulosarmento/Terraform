3ed568651a53a2c8e72998af112bfa6a26c05f61
PauloSarmento <paulo.cesar.sarmento@hotmail.com>
2023-12-23 15:31:57 -0300

    Refatornado subnets

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "new-vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "fullcycle-vpc",
+    Name = "${var.prefix}-vpc",
   }
 }
 
@@ -9,21 +9,29 @@ data "aws_availability_zones" "available" {}
 # output "az" {
 #   value = data.aws_availability_zones.available.names
 # }
-
-
-resource "aws_subnet" "new-subnet-1" {
-  availability_zone = "us-east-1a"
-  vpc_id            = aws_vpc.new-vpc.id
-  cidr_block        = "10.0.1.0/24"
+resource "aws_subnet" "subnets" {
+  count                   = 2
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  vpc_id                  = aws_vpc.new-vpc.id
+  cidr_block              = "10.0.${count.index}.0/24"
+  map_public_ip_on_launch = true
   tags = {
-    Name = "fullcycle-subnet-1",
+    Name = "${var.prefix}-subnet-${count.index}",
   }
 }
-resource "aws_subnet" "new-subnet-2" {
-  availability_zone = "us-east-1b"
-  vpc_id            = aws_vpc.new-vpc.id
-  cidr_block        = "10.0.0.0/24"
-  tags = {
-    Name = "fullcycle-subnet-2",
-  }
-}
+# resource "aws_subnet" "new-subnet-1" {
+#   availability_zone = "us-east-1a"
+#   vpc_id            = aws_vpc.new-vpc.id
+#   cidr_block        = "10.0.1.0/24"
+#   tags = {
+#     Name = "fullcycle-subnet-1",
+#   }
+# }
+# resource "aws_subnet" "new-subnet-2" {
+#   availability_zone = "us-east-1b"
+#   vpc_id            = aws_vpc.new-vpc.id
+#   cidr_block        = "10.0.0.0/24"
+#   tags = {
+#     Name = "fullcycle-subnet-2",
+#   }
+# }
