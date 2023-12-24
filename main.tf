16081c0c0cab570e292368580c45941335f3accb
PauloSarmento <paulo.cesar.sarmento@hotmail.com>
2023-12-24 10:09:36 -0300

    Criando cluster com Modules

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">=0.13.1"
+  required_providers {
+    aws   = ">=5.31.0"
+    local = ">=2.1.0"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
 module "new-vpc" {
   source         = "./modules/vpc"
   prefix         = var.prefix
